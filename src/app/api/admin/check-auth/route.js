4c5bb7a005f261785cd24361e12c9efea340c74a
Mizanur Rahman <mizan.rahman@zhiltd.com>
2026-08-16 12:08:59 +0300

    dynamic auth token

--- src/app/api/admin/check-auth/route.js
+++ src/app/api/admin/check-auth/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { verifyToken } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
   try {
     const token = request.cookies.get('admin_token');
